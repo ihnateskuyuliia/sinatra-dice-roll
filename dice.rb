0c075d5b61c3b801d7f1281c370bab8920841d37
ihnateskuyuliia <yuliiaihnatesku@uchicago.edu>
2024-10-18 20:22:05 +0000

    fixed 2/6 page

--- dice.rb
+++ dice.rb
@@ -8,7 +8,16 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  erb :home
+  erb(:home)
+end
+
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
 end
 
 # DICE 2 ROLLS 6 SIDES
@@ -17,10 +26,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 # DICE 2 ROLL 10 SIDES
